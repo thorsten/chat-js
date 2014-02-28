248f103c0d27408fa025f725f23810231d8ee287
Thorsten Rinne <t.rinne@yatego.com>
2014-02-28 11:43:13 +0100

    Fixed indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,113 +1,113 @@
 module.exports = function (grunt) {
-  var config = {
-    copy: {
-      main: {
-        src: 'public/vendor/jquery/dist/jquery.min.js',
-        dest: 'public/js/jquery.min.js'
-      }
-    },
-    jshint: {
-      options: {
-        ignores: ['node_modules/**', 'public/vendor/**', '**/*.min.js'],
-        jshintrc: '.jshintrc'
-      },
-      gruntfile: 'Gruntfile.js',
-      server: ['controllers/**/*.js', 'models/**/*.js', 'routes/**/*.js', 'app.js', 'config.js'],
-      client: 'public/**/*.js'
-    },
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        files: {
-          'public/js/app.min.js': ['src/*.js']
-        }
-      }
-    },
-    less: {
-      development: {
-        options: {
-          paths: ['public/css']
+    var config = {
+        copy: {
+            main: {
+                src: 'public/vendor/jquery/dist/jquery.min.js',
+                dest: 'public/js/jquery.min.js'
+            }
         },
-        files: {
-          'public/css/styles.css': 'public/less/styles.less'
-        }
-      },
-      production: {
-        options: {
-          paths: ['public/css'],
-          cleancss: true
+        jshint: {
+            options: {
+                ignores: ['node_modules/**', 'public/vendor/**', '**/*.min.js'],
+                jshintrc: '.jshintrc'
+            },
+            gruntfile: 'Gruntfile.js',
+            server: ['controllers/**/*.js', 'models/**/*.js', 'routes/**/*.js', 'app.js', 'config.js'],
+            client: 'public/**/*.js'
         },
-        files: {
-          'public/css/styles.css': 'public/less/styles.less'
-        }
-      }
-    },
-    cssmin: {
-      addBanner: {
-        options: {
-          banner: '/* (c) 2014 Thorsten Rinne & Sebastian Springer */'
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            dist: {
+                files: {
+                    'public/js/app.min.js': ['src/*.js']
+                }
+            }
         },
-        files: {
-          'public/css/styles.min.css': ['public/css/styles.css']
+        less: {
+            development: {
+                options: {
+                    paths: ['public/css']
+                },
+                files: {
+                    'public/css/styles.css': 'public/less/styles.less'
+                }
+            },
+            production: {
+                options: {
+                    paths: ['public/css'],
+                    cleancss: true
+                },
+                files: {
+                    'public/css/styles.css': 'public/less/styles.less'
+                }
+            }
+        },
+        cssmin: {
+            addBanner: {
+                options: {
+                    banner: '/* (c) 2014 Thorsten Rinne & Sebastian Springer */'
+                },
+                files: {
+                    'public/css/styles.min.css': ['public/css/styles.css']
+                }
+            }
+        },
+        'node-inspector': {
+            options: {
+                'save-live-edit': true
+            }
+        },
+        nodemon: {
+            dev: {
+                script: 'app.js',
+                options: {
+                    nodeArgs: ['--debug'],
+                    cwd: __dirname,
+                    ignore: ['node_modules/', 'public/'],
+                    ext: 'js',
+                    watch: ['controllers/**/*.js', 'models/**/*.js', 'routes/**/*.js', 'app.js', 'config.js'],
+                    delay: 1,
+                    legacyWatch: true
+                }
+            }
+        },
+        watch: {
+            all: {
+                files: ['public/**/*', 'views/**', '!**/node_modules/**', '!public/vendor/**/*', '!**/*.min.*'],
+                options: {
+                    livereload: 3006
+                }
+            },
+            gruntfile: {
+                files: 'Gruntfile.js',
+                tasks: 'jshint:gruntfile'
+            },
+            scripts: {
+                files: 'public/js/**/*.js',
+                tasks: 'jshint:client'
+            },
+            server: {
+                files: ['controllers/**/*.js', 'models/**/*.js', 'routes/**/*.js', 'app.js', 'config.js'],
+                tasks: 'jshint:server'
+            },
+            less: {
+                files: ['public/less/**/*.less'],
+                tasks: ['less', 'cssmin']
+            }
+        },
+        concurrent: {
+            tasks: ['nodemon', 'node-inspector', 'watch'],
+            options: {
+                logConcurrentOutput: true
+            }
         }
-      }
-    },
-    'node-inspector': {
-      options: {
-        'save-live-edit': true
-      }
-    },
-    nodemon: {
-      dev: {
-        script: 'app.js',
-        options: {
-          nodeArgs: ['--debug'],
-          cwd: __dirname,
-          ignore: ['node_modules/', 'public/'],
-          ext: 'js',
-          watch: ['controllers/**/*.js', 'models/**/*.js', 'routes/**/*.js', 'app.js', 'config.js'],
-          delay: 1,
-          legacyWatch: true
-        }
-      }
-    },
-    watch: {
-      all: {
-        files: ['public/**/*', 'views/**', '!**/node_modules/**', '!public/vendor/**/*', '!**/*.min.*'],
-        options: {
-          livereload: 3006
-        }
-      },
-      gruntfile: {
-        files: 'Gruntfile.js',
-        tasks: 'jshint:gruntfile'
-      },
-      scripts: {
-        files: 'public/js/**/*.js',
-        tasks: 'jshint:client'
-      },
-      server: {
-        files: ['controllers/**/*.js', 'models/**/*.js', 'routes/**/*.js', 'app.js', 'config.js'],
-        tasks: 'jshint:server'
-      },
-      less: {
-        files: ['public/less/**/*.less'],
-        tasks: ['less', 'cssmin']
-      }
-    },
-    concurrent: {
-      tasks: ['nodemon', 'node-inspector', 'watch'],
-      options: {
-        logConcurrentOutput: true
-      }
-    }
-  };
+    };
 
-  grunt.initConfig(config);
+    grunt.initConfig(config);
 
-  // Load the tasks
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-  grunt.registerTask('default', ['copy', 'less', 'cssmin', 'uglify', 'concurrent']);
+    // Load the tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    grunt.registerTask('default', ['copy', 'less', 'cssmin', 'uglify', 'concurrent']);
 };
