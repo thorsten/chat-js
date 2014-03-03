4efd5e52a63c52d80d746c1445e0e188ff927e31
Sebastian Springer <sebastian.springer@mayflower.de>
2014-03-03 08:05:27 +0100

    fixed geolocation

--- src/geolocation.js
+++ src/geolocation.js
@@ -8,10 +8,10 @@ var getDistance = function (lat1, lon1, lat2, lon2) {
     'use strict';
 
     var R = 6371; // km
-    var dLat = (lat2 - lat1).toRad();
-    var dLon = (lon2 - lon1).toRad();
+    var dLat = toRad(lat2 - lat1);
+    var dLon = toRad(lon2 - lon1);
     var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+            Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
             Math.sin(dLon / 2) * Math.sin(dLon / 2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
